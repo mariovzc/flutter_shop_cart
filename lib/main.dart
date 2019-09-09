13d91ceb90806b41807b3f3688deb58200664ad3
mariovzc <mario.vizcainos@gmail.com>
2019-09-09 10:52:50 -0500

    initial files

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueAccent[400],
+        accentColor: Colors.blueGrey,
+      ),
+      home: ProductsOverViewScreen(),
+    );
+  }
+}
