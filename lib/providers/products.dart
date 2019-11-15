a22837bddd1c9ec6c629d2d52aad1d5e7c5ab8d8
mariovzc <mario.vizcainos@gmail.com>
2019-11-15 10:53:23 -0500

    handle delete and custom dart error exceptions

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:shop/models/http_exception.dart';
 import 'package:shop/providers/product.dart';
 import 'package:http/http.dart' as http;
 class Products with ChangeNotifier{
@@ -136,8 +137,19 @@ class Products with ChangeNotifier{
     }
   }
 
-  void deleteProduct(String id){
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async{
+    final _url = 'https://products-flutter-example.firebaseio.com/products/$id';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    Product existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(_url);
+      if (response.statusCode >= 400) {
+        _items.insert(existingProductIndex, existingProduct);
+        notifyListeners();
+        throw HttpException('Could not delete a Product');
+      } else {
+        existingProduct = null;
+      }
   } 
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -18,6 +18,7 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -36,11 +37,22 @@ class UserProductItem extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: (){
-                Provider.of<Products>(
-                  context,
-                  listen: false
-                ).deleteProduct(id);
+              onPressed: () async{
+                try {
+                  await Provider.of<Products>(
+                    context,
+                    listen: false
+                  ).deleteProduct(id);
+                } catch(e) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        e.toString(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }
               },
               color: Theme.of(context).errorColor,
             ),
