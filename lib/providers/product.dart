e9a4064849371c6d8b5700eaa8456cf665d126af
mariovzc <mario.vizcainos@gmail.com>
2019-11-18 10:29:04 -0500

    handle update of favStatus

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 class Product  with ChangeNotifier{
   final String id;
   final String title;
@@ -17,8 +19,29 @@ class Product  with ChangeNotifier{
     this.isFavorite = false
   });
 
-  void toggleFavoriteStatus() {
+
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final _url = 'https://products-flutter-example.firebaseio.com/products/$id';
+    try {
+      final response = await http.patch(
+        _url,
+        body: json.encode({
+          'isFavorite': isFavorite
+        }),
+      );
+      if(response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch(e) {
+      _setFavValue(oldStatus);
+    }
   }
 }
